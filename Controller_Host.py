e61e2453588bd3f8bea0f81010b577fdcf849563
Cameron Charles <cjch444@outlook.com>
2022-02-15 11:28:04 -0500

    testing some stuff

--- Controller_Host.py
+++ Controller_Host.py
@@ -1,44 +1,37 @@
-import os
-import time
-from timeit import default_timer as timer
-import socket,cv2, pickle,struct
-import subprocess
+import socket
+import cv2
 import controller_util
 
 IP = socket.gethostname()
 PORT = 4450
 print(IP)
+ADDR = (IP, PORT)
 def video_handler():
 
     # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
     # all interfaces)
-    server_socket = socket.socket()
-    server_socket.bind(('0.0.0.0', 8000))
-    server_socket.listen(0)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # used IPV4 and TCP connection
+    server.bind(ADDR)
+    server.listen(3)
 
     # Accept a single connection and make a file-like object out of it
-    connection = server_socket.accept()[0].makefile('rb')
+    connection = server.accept()[0].makefile('rb')
     try:
-        # Run a viewer with an appropriate command line. Uncomment the mplayer
-        # version if you would prefer to use mplayer instead of VLC
-        cmdline = ['vlc', '--demux', 'h264', '-']
-        #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
-        player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
         while True:
             # Repeatedly read 1k of data from the connection and write it to
             # the media player's stdin
             data = connection.read(1024)
             if not data:
                 break
-            player.stdin.write(data)
+            else:
+                cv2.imshow('Frame', data)
+
     finally:
         connection.close()
-        server_socket.close()
-        player.terminate()
+        server.close()
 
 
 def client_handler():
-    ADDR = (IP, PORT)
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # used IPV4 and TCP connection
     server.bind(ADDR)
     server.listen(3)
