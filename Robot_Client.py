f55d440d91d0e69d7cca3eb0c16124390af55f38
Cameron Charles <cjch444@outlook.com>
2022-04-05 17:25:25 -0400

    cummit3

--- Robot_Client.py
+++ Robot_Client.py
@@ -156,8 +156,10 @@ def compass_handler(client):
         if (heading < 0):  # Ensure the angle is positive
             heading = heading + 2 * pi
 
-        userHeadingAngle = str(heading * (180 / pi))  # converting into the heading angle
-        client.send(userHeadingAngle.encode('utf-8'))
+        userHeadingAngle = int(heading * (180 / pi))  # converting into the heading angle
+        print("Current Heading Angle = %d°" % userHeadingAngle)
+        string_to_send = "%d" % userHeadingAngle
+        client.send(string_to_send.encode('utf-8'))
 
         time.sleep(0.01)  # sets a short wait before re-looping
 
